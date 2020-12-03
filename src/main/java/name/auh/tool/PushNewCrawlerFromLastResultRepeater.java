4b8031a7dbdbed1908653f1feb92166796d17294
fanlushuai <fanlushuai@github.com>
2020-12-04 06:01:41 +0800

    重构 重写流量控制，由多个中级控制，变为一个总体请求流量控制

--- src/main/java/name/auh/tool/PushNewCrawlerFromLastResultRepeater.java
+++ src/main/java/name/auh/tool/PushNewCrawlerFromLastResultRepeater.java
@@ -2,10 +2,13 @@ package name.auh.tool;
 
 import cn.wanghaomiao.seimi.spring.common.CrawlerCache;
 import cn.wanghaomiao.seimi.struct.Request;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 上步结果和下次抓取  中继器
@@ -13,38 +16,48 @@ import java.util.concurrent.ArrayBlockingQueue;
  * 这个写法也不是最优的。但是最简单的。最优的写法可以动态的生成队列和定时任务去调度。不用每次新增任务中继度进行代码新增
  */
 @Component
+@Slf4j
 public class PushNewCrawlerFromLastResultRepeater {
 
     final static ArrayBlockingQueue<Request> FORK_LIST_RESULT = new ArrayBlockingQueue(1314520);
 
     final static ArrayBlockingQueue<Request> FORK_REPO_RESULT = new ArrayBlockingQueue(1314520);
 
-    /**
-     * 定时获取数据 从forklist爬取产生的结果队列 ，进行forkRepo的进一步抓取操作
-     * 可以控制多长时间，获取多少个结果，来控制抓取流量，进一步消除ratelimit
-     */
-    @Scheduled(fixedDelay = 2000)
-    public void pushForkRepoFromForkListResult() {
-        pushCrawlerFromLastResultRequestQueue(FORK_LIST_RESULT, 5, "ForkModifySearch");
-    }
+    private final static AtomicInteger REQUEST_COUNT =new AtomicInteger(0);
 
     /**
-     * 定时获取数据 从forkRepo爬取产生的结果队列 ，进行ForkRepoCommitLog的进一步抓取操作
-     * 可以控制多长时间，获取多少个结果，来控制抓取流量，进一步消除ratelimit
+     * 总体 抓取请求并发控制
+     * @throws IllegalAccessException
      */
     @Scheduled(fixedDelay = 3000)
-    public void pushForkRepoCommitLogFromForkRepoResult() {
-        pushCrawlerFromLastResultRequestQueue(FORK_REPO_RESULT, 5, "ForkModifySearch");
+    public void controlRequest() throws IllegalAccessException {
+        int oneTimeRequestCount=10;
+        Field[] fields=PushNewCrawlerFromLastResultRepeater.class.getDeclaredFields();
+        for (Field field : fields) {
+           if(!field.getType().equals(ArrayBlockingQueue.class)){
+               continue;
+           }
+            field.setAccessible(true);
+            ArrayBlockingQueue<Request> arrayBlockingQueue=(ArrayBlockingQueue<Request>)field.get(this);
+            oneTimeRequestCount=pushCrawlerFromLastResultRequestQueue(arrayBlockingQueue,oneTimeRequestCount,"ForkModifySearch");
+            if(oneTimeRequestCount==0){
+                return;
+            }
+        }
     }
 
-    private void pushCrawlerFromLastResultRequestQueue(ArrayBlockingQueue<Request> lastResultRequestQueue, int oneTimePollCount, String crawlerName) {
+    private int pushCrawlerFromLastResultRequestQueue(ArrayBlockingQueue<Request> lastResultRequestQueue, int oneTimePollCount, String crawlerName) {
+        int leftTimes= oneTimePollCount;
         for (int i = 0; i < oneTimePollCount; i++) {
             Request request = lastResultRequestQueue.poll();
             if (request == null) {
-                break;
+                return leftTimes;
             }
             CrawlerCache.getCrawlerModel(crawlerName).sendRequest(request);
+            log.debug("request Counter # {}", REQUEST_COUNT.incrementAndGet());
+            leftTimes-=1;
         }
+        return leftTimes;
     }
 
 
